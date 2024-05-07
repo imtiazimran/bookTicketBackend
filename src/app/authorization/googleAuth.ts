ecadfe67f295290630b51b7330062ce3cb164f37
imtiazimran <imtiazimran@outlook.com>
2024-05-07 22:13:49 +0600

    s in the  redirect uri

--- src/app/authorization/googleAuth.ts
+++ src/app/authorization/googleAuth.ts
@@ -18,7 +18,7 @@ var corsOptions = {
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID as string,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    callbackURL: '/auth/google/callback'
+    callbackURL: 'https://bookticketbackend.onrender.com/auth/google/callback'
 }, async (accessToken, refreshToken, profile, done) => {
     const user = profile._json;
 
